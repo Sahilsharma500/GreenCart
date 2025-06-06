c385271b80230157d660e8d1875dcc4fc65beb8e
Sahilsharma500 <4sahilsharma1@gmail.com>
2025-06-06 10:40:22 +0530

    user model defined and user controller initiated

---
+++ server/controllers/userController.js
@@ -0,0 +1,21 @@
+import User from "../models/User.js";
+
+//Register user :/api/user/register
+export const register = async(req, res) => {
+    try{
+        const {name, email,password} = req.body;
+        if(!name || !email ||!password){
+            return res.json({success: false, message: 'Missing Details'})
+        }
+
+        const existingUser = await User.findOne({email})
+
+        if(existingUser){
+            return res.json({success: false, message: 'User already exists'})
+        }
+        
+    }
+    catch(error){
+
+    }
+}
